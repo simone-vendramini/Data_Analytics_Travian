99ea1ab0885646ebac8d9e81282178b9ece16183
Simone Vendramini <102531922+Svendra4UniMiB@users.noreply.github.com>
2024-05-31 16:38:25 +0200

    abbellimento istogrammi

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -26,45 +26,57 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets) #default
 app.layout = html.Div(children=[
     html.H1(children='Travian Dashboard'),
     html.Div([
+        html.Div(['Istogramma differenze dei gradi dei nodi calcolato e fornito'], style={'text-align': 'center', 'textColor': 'darkslategray', 'fontSize': 20, 'margin': '10px'}),
         html.Div([
             html.Div([
-                dcc.Dropdown(
-                    id='type-degree-dropdown',
-                    options=[
-                        {'label': 'Outdegree', 'value': 'outdegree'},
-                        {'label': 'Indegree', 'value': 'indegree'}
-                    ],
-                    value='outdegree',
-                    style={'width': '100%'},
-                    clearable=False
-                ),
-            ], style={'width': '48%', 'display': 'inline-block'}),
-            html.Div([
-                dcc.Dropdown(
-                    id='type-graph-dropdown',
-                    options=[
-                        {'label': 'Attack', 'value': 'attack'},
-                        {'label': 'Trade', 'value': 'trade'},
-                        {'label': 'Message', 'value': 'message'},
-                    ],
-                    value='attack',
-                    style={'width': '100%'},
-                    clearable=False
-                ),
-            ], style={'width': '48%', 'display': 'inline-block', 'marginLeft': '4%'}),
-        ], style={'text-align': 'center'}),
-        
-        html.Div(children='Selezione giorno', style={'text-align': 'center'}),
+                html.Div([
+                    dcc.Dropdown(
+                        id='type-degree-dropdown',
+                        options=[
+                            {'label': 'Outdegree', 'value': 'outdegree'},
+                            {'label': 'Indegree', 'value': 'indegree'}
+                        ],
+                        value='outdegree',
+                        style={'width': '100%'},
+                        clearable=False
+                    ),
+                ], style={'width': '48%', 'display': 'inline-block'}),
+                html.Div([
+                    dcc.Dropdown(
+                        id='type-graph-dropdown',
+                        options=[
+                            {'label': 'Attack', 'value': 'attack'},
+                            {'label': 'Trade', 'value': 'trade'},
+                            {'label': 'Message', 'value': 'message'},
+                        ],
+                        value='attack',
+                        style={'width': '100%'},
+                        clearable=False
+                    ),
+                ], style={'width': '48%', 'display': 'inline-block', 'marginLeft': '4%'}),
+            ], style={'text-align': 'center'}),
+            
+            html.Div(children='Selezione giorno', style={'text-align': 'center'}),
 
-        dcc.Slider(
-            id='day-slider',
-            min=1,
-            max=30,
-            value=10,
-            marks={i: str(i) for i in range(1, 31)},
-            step=1
-        ),
-        dcc.Graph(id='histogram-graph')
+            dcc.Slider(
+                id='day-slider',
+                min=1,
+                max=30,
+                value=10,
+                marks={i: str(i) for i in range(1, 31)},
+                step=1
+            ),
+            dcc.Graph(id='histogram-graph'),
+            html.Div(
+                id='hover-data',
+                style={'padding': '10px', 'margin': '10px', 'text-align': 'center'}
+            )
+        ], style={
+            'border': '2px solid darkslategray',
+            'padding': '10px',
+            'margin': '10px',
+            'border-radius': '15px'
+            })
     ], style={'width': '80%', 'margin': 'auto'})
 
 ])
@@ -75,7 +87,6 @@ app.layout = html.Div(children=[
     Input('type-graph-dropdown', 'value'),
     Input('day-slider', 'value'),
 )
-
 def update_histogram(type_degree, type_graph, day):
     day -= 1
     if type_graph == 'attack':
@@ -99,7 +110,7 @@ def update_histogram(type_degree, type_graph, day):
 
     layout = go.Layout(
         xaxis=dict(title='Scarto ' + type_degree),
-        yaxis=dict(title='Logaritmo della frequenza dei nodi', type='log'),
+        yaxis=dict(title='Frequenza dei nodi', type='log'),
         bargap=0.1,
         bargroupgap=0.1
     )
@@ -107,6 +118,20 @@ def update_histogram(type_degree, type_graph, day):
     fig = go.Figure(data=[hist_data], layout=layout)
     
     return fig
+
+@app.callback(
+    Output('hover-data', 'children'),
+    Input('histogram-graph', 'hoverData')
+)
+def display_hover_data(hoverData):
+    if hoverData is None:
+        return "Passa il mouse sopra una colonna per vedere le informazioni."
+    
+    # Estrai le informazioni dal hoverData
+    point = hoverData['points'][0]
+    category = point['x']
+    value = point['y']
+    return f"Ci sono {value} nodi con una differenza di {category} gradi rispetto alle informazioni fornite."
 
 if __name__ == '__main__':
     app.run_server(debug=True)
